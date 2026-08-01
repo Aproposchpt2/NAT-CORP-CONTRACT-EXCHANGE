923c18b90b373b6b9643358dcf36f65d55451ec7
Aproposchpt2 <157073040+Aproposchpt2@users.noreply.github.com>
2026-08-01 16:55:20 -0700

    Remove failing function route for OTF page

--- netlify/functions/opportunity-fulfillment-page.mjs
+++
@@ -1,8 +0,0 @@
-export default async function handler(req) {
-  const url = new URL(req.url);
-  const target = new URL('/opportunity-fulfillment.html', url.origin);
-  target.search = url.search;
-  return Response.redirect(target.toString(), 302);
-}
-
-export const config = { path: '/opportunity-fulfillment' };
